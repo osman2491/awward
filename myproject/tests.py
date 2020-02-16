79f8d433164bd7743632cecbbad944eb2f7c0b5c
osman67239121@gmail.com <osman67239121@gmail.com>
2020-02-17 02:18:26 +0300

    add test update and get single post information methods

--- myproject/tests.py
+++ myproject/tests.py
@@ -12,3 +12,14 @@ class PosTestClass(TestCase):
 
     def test_instance(self):
         self.assertTrue(isinstance(self.post,Post))
+
+    def test_save_method(self):
+        self.post.save_post()
+        post = Post.objects.all()
+        self.assertTrue(len(post)>0)
+    
+    def test_delete_method(self):
+        self.post.save_post()
+        self.post.delete_post()
+        post = Post.objects.all()
+        self.assertTrue(len(post) is 0)
