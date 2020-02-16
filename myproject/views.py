8b5111aeac7dc2a3f84f2b82925970e6a638c9b3
osman67239121@gmail.com <osman67239121@gmail.com>
2020-02-17 01:09:12 +0300

    add models to admin site

--- myproject/views.py
+++ myproject/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from . models import *
+from .forms import ProjectUpload,UpdateProfileForm
+from django.contrib.auth.decorators import login_required
+from rest_framework import viewsets
+from .serializers import PostSerializer,ProfileSeralizer
+
 
 
 def home(request):
