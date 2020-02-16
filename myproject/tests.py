cdb6b6750468132ca614cebee1c63f81a76b8505
osman67239121@gmail.com <osman67239121@gmail.com>
2020-02-17 02:16:54 +0300

    add test to test delete and save post methods

--- myproject/tests.py
+++ myproject/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import *
 
-# Create your tests here.
+
+class PosTestClass(TestCase):
+    '''
+    This is a class that perform unittest on the Post Model.
+    '''
+    
+    def setUp(self):
+        self.post = Post(id=1,title='new post',image='images/lagoon.jpeg',description='the best of the best', link='https://link.com/',user_id=3)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.post,Post))
