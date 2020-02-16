ede8e9b96cd2e9e9158eae07a89f682d8a59aa53
osman67239121@gmail.com <osman67239121@gmail.com>
2020-02-17 01:03:07 +0300

    add project update form

--- myproject/forms.py
+++ myproject/forms.py
@@ -5,3 +5,7 @@ class ProjectUpload(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title','image','description','link')
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
