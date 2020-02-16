0fb3dad51bbd52e2d40f9136be17eb945a3df97b
osman67239121@gmail.com <osman67239121@gmail.com>
2020-02-17 00:59:02 +0300

    add method to view new projects

--- myproject/views.py
+++ myproject/views.py
@@ -6,3 +6,17 @@ def home(request):
     projects = Post.objects.all()
     return render(request,'myprojects/index.html',{"projects":projects})
 
+@login_required (login_url='/accounts/login/?next=/')   
+def new_project(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProjectUpload(request.POST,request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.user = current_user
+            project.save()
+        return redirect('home')
+    else:
+        form = ProjectUpload()
+        return render(request,'myprojects/new_post.html',{"form":form})
+
