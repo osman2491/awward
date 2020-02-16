275cae7890cc97b341c1946c8f30b2b4a31524a2
osman67239121@gmail.com <osman67239121@gmail.com>
2020-02-17 02:19:38 +0300

    add class to test profile methods

--- myproject/tests.py
+++ myproject/tests.py
@@ -23,3 +23,22 @@ class PosTestClass(TestCase):
         self.post.delete_post()
         post = Post.objects.all()
         self.assertTrue(len(post) is 0)
+
+    def tearDown(self):
+        Post.objects.all().delete() 
+
+class ProfileTestClass(TestCase):
+    '''
+    This is a class that perform unittest on the Profile Model.
+    '''
+    
+    def setUp(self):
+        self.profile = Profile(First_Name='daniel',Last_Name='karani',Email='danielkarani@gmail.com', bio='test bio',user_id=3)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile,Profile))
+        
+    def test_save_method(self):
+        self.profile.save_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile)>0)
