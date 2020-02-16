87fb10007958eb91c3b7bfbd290dd05282ce37e8
osman67239121@gmail.com <osman67239121@gmail.com>
2020-02-17 00:43:09 +0300

    add profile model

--- myproject/models.py
+++ myproject/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import datetime as dt 
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
-# Create your models here.
+class Profile(models.Model):
+    First_Name = models.CharField(max_length=50)
+    Last_Name = models.CharField(max_length=50)
+    Email = models.EmailField(max_length=50)
+    bio = HTMLField()
+    profile_pic = models.ImageField(upload_to='images/' , default='images/smoke.jpeg')
+    user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
